bf363306759ee4598a8d2754b8d07433481a23e8
Brysen Ackx <brysen.ackx@gmail.com>
2018-07-10 10:31:15 +0200

    Addded authentication
    Added basic API authentication
    /api/login
    /api/register
    /api/details

--- config/app.php
+++ config/app.php
@@ -160,6 +160,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        Laravel\Passport\PassportServiceProvider::class,
     ],
 
     /*
