960fc8dcd39fd3e68d056588ecea1dfb36316290
Brysen Ackx <9168041+awildbrysen@users.noreply.github.com>
2018-07-16 16:29:24 +0200

    Bugfix on upload (#24)
    * Merge w/ current develop
    
    * Everything works
    
    * fixed bug

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -56,7 +56,7 @@ class ProjectController extends Controller
         foreach ($tagNames as $tagName) {
             $tagName = trim($tagName);
             $tag = Tag::where('name', $tagName)->first();
-            if (!$tag->count()) {
+            if ($tag) {
                 $tag = new Tag;
                 $tag->name = $tagName;
                 $tag->save();
