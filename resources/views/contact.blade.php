28b0544bd5139937670cdc69170df966bc5be305
noxoor <noxoor@gmail.com>
2018-07-23 15:46:15 +0200

    Contact Page OK

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -2,10 +2,9 @@
 
 @section('content')
     <section class="contact">
-        <form class="contact__form" action="submited">
-            <div class="animation-container">
-                <!-- Step one -->
-                <div class="form-step-1 form-step js-form-step" data-step="1">
+        <section class="contact__form">
+            <form class="contact__form" action="submited">
+                <div class="animation-container">
                     <h1 class="contact__title u-title type-1"> Contact us</h1>
                     <p class="contact__desc">
                         If you have any questions, suggestions or need help, please contact us via the form below.
@@ -43,10 +42,34 @@
                             <button type="submit" class="c-button btn">Send</button>
                         </div>
                     </div>
+                </div>
 
+            </form>
+        </section>
+        <section class="contact__infos">
+            <div class="animation-container">
+                <h1 class="contact__title u-title type-1"> Contact information</h1>
+                <p class="contact__desc">
+                    If you have any questions, suggestions or need help, please contact us via the form below.
+                </p>
+                <div class="contact__item">
+                    <h2 class="u-title type-3">Adress</h2>
+                    <ul>
+                        <li>Herman Teirlinnckgebouw B</li>
+                        <li>Avenue du Port 88</li>
+                        <li>1000 Brussels</li>
+                    </ul>
+                </div>
+                <div class="contact__item">
+                    <h2 class="u-title type-3">Email</h2>
+                    <a href="mailto:hilde.luyts@vlaanderen.be">hilde.luyts@vlaanderen.be</a>
+                </div>
+                <div class="contact__item">
+                    <h2 class="u-title type-3">Tel.</h2>
+                    <a href="tel:(+32)2 553 83 25">(+32)(0)2 553 83 25</a>
                 </div>
             </div>
-        </form>
+        </section>
     </section>
 
 @endsection
