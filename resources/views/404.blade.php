64b43367b2ed3c7655243023a346cfb42bf657c3
noxoor <noxoor@gmail.com>
2018-07-25 11:42:50 +0200

    404 Page

--- resources/views/404.blade.php
+++ resources/views/404.blade.php
@@ -2,8 +2,10 @@
 
 @section('content')
     <section class="error">
-        <h1 class="error__title">404</h1>
-        <p>Take me back to <a href="{{"/"}}">openflandria.be</a></p>
+        <div class="error__wrap">
+            <h1 class="error__title">404</h1>
+            <p>Take me back to <a href="{{"/"}}">openflandria.be</a></p>
+        </div>
     </section>
 
 @endsection
